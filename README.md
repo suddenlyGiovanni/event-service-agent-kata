0d6404acadc8f8b4be6251bb4980a2906b1a614f
suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>
2025-09-19 17:26:10 +0200

    chore(env): add .envrc and update .gitignore
    Create a new .envrc file to use flake and update .gitignore to exclude
    unnecessary directories and files.
    
    Signed-off-by: suddenlyGiovanni <15946771+suddenlyGiovanni@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# event-service-agent
+# event-service-agent-kata
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs-slim_24
+            pnpm
+          ];
+        };
+      });
+    };
+}
